e0b1f6084626388ca13566f967ed58d63b8ddf7e
미플 <devmeeple@gmail.com>
2023-12-15 05:06:45 +0900

    feat: 2장. 타락한 객체

--- src/dollar.test.ts
+++ src/dollar.test.ts
@@ -1,14 +1,13 @@
 import { Dollar } from './dollar';
 
-describe('1. 다중통화구현', () => {
-  it('곱셈', () => {
-    // given
+describe('2. 타락한 객체', () => {
+  it('Dollar 클래스 부작용 처리하기', () => {
     const five = new Dollar(5);
+    let product = five.times(2);
 
-    // when
-    five.times(2);
+    expect(product.amount).toBe(10);
 
-    // then
-    expect(five.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });

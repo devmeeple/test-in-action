7c6645d3297c6bb6f9b134eecdc776cc3ac03f45
미플 <devmeeple@gmail.com>
2024-04-19 20:05:36 +0900

    [테스트 주도 개발] 03. 모두를 위한 평등
    - VO란 무엇인가

--- src/tdd-by-example/Dollar.ts
+++ src/tdd-by-example/Dollar.ts
@@ -4,4 +4,9 @@ export class Dollar {
   times(multiplier: number) {
     return new Dollar(this.amount * multiplier);
   }
+
+  // 값 객체(Value Object) 동등성 비교
+  equals(other: object) {
+    return this.amount === (other as Dollar).amount;
+  }
 }
--- test/tdd-by-example/Money.spec.ts
+++ test/tdd-by-example/Money.spec.ts
@@ -11,4 +11,9 @@ describe('Money', () => {
     // then
     expect(product.amount).toBe(10);
   });
+
+  it('같은 값을 가진 객체(VO)는 동등해야 한다', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+  });
 });

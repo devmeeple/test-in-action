cb7fc0dc803d808fa895869383d5af4de55d320a
미플 <devmeeple@gmail.com>
2024-05-24 13:01:37 +0900

    refactor: totalAmount 변수 제거하기

--- src/refactoring/chapter1/statement.ts
+++ src/refactoring/chapter1/statement.ts
@@ -1,6 +1,20 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  for (const perf of invoice.performances) {
+    // 청구 내역 출력
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (const perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
@@ -45,21 +59,11 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  for (const perf of invoice.performances) {
-    // 청구 내역 출력
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
-  }
-
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (const perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
-
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 }

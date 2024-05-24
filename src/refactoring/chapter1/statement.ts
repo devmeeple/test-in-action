4d80c5ac063b22b26fc22aba0c836ac32441a91a
미플 <devmeeple@gmail.com>
2024-05-24 12:28:07 +0900

    refactor: format 변수 제거하기

--- src/refactoring/chapter1/statement.ts
+++ src/refactoring/chapter1/statement.ts
@@ -2,11 +2,14 @@ export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const formant = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
+  }
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -47,10 +50,10 @@ export function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${formant(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${formant(totalAmount / 100)}\n`;
+  result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }

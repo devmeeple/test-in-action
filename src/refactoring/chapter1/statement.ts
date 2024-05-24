1d5a9596b308efc4964cc85b6456a30d1dea6a30
미플 <devmeeple@gmail.com>
2024-05-24 12:42:05 +0900

    refactor: 함수 선언 바꾸기(format -> usd)

--- src/refactoring/chapter1/statement.ts
+++ src/refactoring/chapter1/statement.ts
@@ -3,12 +3,12 @@ export function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   function amountFor(aPerformance) {
@@ -50,10 +50,10 @@ export function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }

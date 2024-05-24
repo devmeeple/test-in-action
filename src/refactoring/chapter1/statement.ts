554db99a232a08cc4774b042527d00f7ff928874
미플 <devmeeple@gmail.com>
2024-05-24 12:50:30 +0900

    refactor: volumeCredits 변수 제거하기

--- src/refactoring/chapter1/statement.ts
+++ src/refactoring/chapter1/statement.ts
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   function usd(aNumber) {
@@ -47,13 +46,20 @@ export function statement(invoice, plays) {
   }
 
   for (const perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역 출력
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (const perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }

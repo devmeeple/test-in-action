515db45889f629eb1158e536074ece271542e2a9
미플 <devmeeple@gmail.com>
2024-08-19 14:01:55 +0900

    refactor(javacan): 코드 정리: 테스트 코드 정리
    - TODO: 검증 필요한 부분

--- test/javacan/ch02/password-strength-meter.spec.ts
+++ test/javacan/ch02/password-strength-meter.spec.ts
@@ -26,16 +26,20 @@ enum PasswordStrength {
 }
 
 describe('PasswordStrengthMeterTest', () => {
+  const meter = new PasswordStrengthMeter();
+  const expectStrength = (password: string, expStr: PasswordStrength) => {
+    const sut = meter.meter(password);
+    expect(sut).toBe(expStr);
+  };
+
   describe('규칙을 모두 만족하면 암호는 강함이다. [성공]', () => {
     it.each([['ab12!@AB'], ['abc1!Add']])('Password: %s', (password) => {
       // given
-      const meter = new PasswordStrengthMeter();
 
       // when
-      const sut = meter.meter(password);
 
       // then
-      expect(sut).toBe(PasswordStrength.STRONG);
+      expectStrength(password, PasswordStrength.STRONG);
     });
   });
 
@@ -44,26 +48,22 @@ describe('PasswordStrengthMeterTest', () => {
       '길이가 8글자 미만이고 나머지 조건은 만족한다. Password: %s',
       (password) => {
         // given
-        const meter = new PasswordStrengthMeter();
 
         // when
-        const sut = meter.meter(password);
 
         // then
-        expect(sut).toBe(PasswordStrength.NORMAL);
+        expectStrength(password, PasswordStrength.NORMAL);
       },
     );
-  });
 
-  it('숫자를 포함하지 않고 나머지 조건은 만족한다.', () => {
-    // given
-    const meter = new PasswordStrengthMeter();
-    const password = 'ab!@ABqwer';
+    it('숫자를 포함하지 않고 나머지 조건은 만족한다.', () => {
+      // given
+      const password = 'ab!@ABqwer';
 
-    // when
-    const sut = meter.meter(password);
+      // when
 
-    // then
-    expect(sut).toBe(PasswordStrength.NORMAL);
+      // then
+      expectStrength(password, PasswordStrength.NORMAL);
+    });
   });
 });
